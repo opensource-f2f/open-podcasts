3836299b9e6015ec69fbf53684b125e17b7fce6b
Rick <1450685+LinuxSuRen@users.noreply.github.com>
2022-03-10 15:21:29 +0800

    Support to show the external link of the podcasts (#27)
    * Support multiple pages feature
    
    * Support to show the link of the podcasts

--- ui/src/Pages/Profile.js
+++ ui/src/Pages/Profile.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import $ from 'jquery'
 import './Profile.css'
-import avatar from './images/img_avatar.png'
+import avatar from '../images/img_avatar.png'
 import Modal from 'react-modal'
-import GlobalAudio from './GlobalAudio'
+import GlobalAudio from '../GlobalAudio'
 import Button from 'cuke-ui/lib/button';
 import Input from 'cuke-ui/lib/input';
 
