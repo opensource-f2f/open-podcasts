a24ccbd9ff0737355db226472d84d7847b088cc5
rick <1450685+LinuxSuRen@users.noreply.github.com>
2022-11-14 17:18:26 +0800

    Support more parameters of the action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,32 @@
 #!/bin/sh
 
-yaml-rss --server $1 > index.rss
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --server=*)
+      server="${1#*=}"
+      ;;
+    --showFile=*)
+      showFile="${1#*=}"
+      ;;
+    --itemsPattern=*)
+      itemsPattern="${1#*=}"
+      ;;
+    --output=*)
+      output="${1#*=}"
+      ;;
+    *)
+      printf "***************************\n"
+      printf "* Error: Invalid argument.*\n"
+      printf "***************************\n"
+      exit 1
+  esac
+  shift
+done
+
+if [ "$showFile" == "" ] || [ "$itemsPattern" == "" ] || [ "$output" == "" ]
+then
+  echo "Flags --showFile, --itemsPattern, --output are required."
+  exit 1
+fi
+
+yaml-rss --server ${server} --show-file ${showFile} --items-pattern ${itemsPattern} > ${output}
